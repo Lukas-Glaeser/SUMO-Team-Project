c04a8e5b7617b708c102a9fad8ed19cc55992d1b
lukas <lukasglaeser2002@gmail.net>
2025-11-26 07:05:10 +0100

    Added getter/setter methods to Edge and Vehicle classes

--- src/main/java/org/example/Object/Edge.java
+++ src/main/java/org/example/Object/Edge.java
@@ -1,4 +1,51 @@
 package org.example.Object;
 
 public class Edge {
+
+    private String ID;
+    private double length;
+    private int laneCount;
+    private int vehicleCount;
+    private double averageSpeed;
+
+    public Edge(String ID){
+        this.ID = ID;
+    }
+
+    public String getID(){
+        return ID;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public int getLaneCount(){
+        return laneCount;
+    }
+
+    public int getVehicleCount(){
+        return vehicleCount;
+    }
+
+    public double getAverageSpeed(){
+        return averageSpeed;
+    }
+
+    public void setLength(double length){
+        this.length = length;
+    }
+
+    public void setLength(int laneCount){
+        this.laneCount = laneCount;
+    }
+
+    public void setVehicleCount(int vehicleCount){
+        this.vehicleCount = vehicleCount;
+    }
+
+    public  void setAverageSpeed(double averageSpeed){
+        this.averageSpeed = averageSpeed;
+    }
+
 }
--- src/main/java/org/example/Object/Vehicle.java
+++ src/main/java/org/example/Object/Vehicle.java
@@ -1,4 +1,73 @@
 package org.example.Object;
 
 public class Vehicle {
+
+    private String ID;
+    private double speed;
+    private double x;
+    private double y;
+    private String type;
+    private String color;
+    private String edgeID;
+
+    public Vehicle(String ID){
+        this.ID = ID;
+    }
+
+    public String getID(){
+        return ID;
+    }
+
+    public double getSpeed(){
+        return speed;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String getEdgeID(){
+        return edgeID;
+    }
+
+    public void setSpeed(double speed){
+        this.speed = speed;
+    }
+/*
+    public void setX(double X){
+        this.x = x;
+    }
+
+    public void setY(double Y){
+        this.y = y;
+    }
+*/
+    public void setPosition(double X,double Y){
+        this.x = X;
+        this.y = Y;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setEdgeID(String edgeID){
+        this.edgeID = edgeID;
+    }
 }
