5bf95d93e10462481db8c13c7dcb7db9c300fbf9
lukas <lukasglaeser2002@gmail.net>
2025-11-26 04:19:01 +0100

    Added getter/setter methods to TrafficLight class and TrafficLight method

--- src/main/java/org/example/Object/TrafficLight.java
+++ src/main/java/org/example/Object/TrafficLight.java
@@ -1,4 +1,25 @@
 package org.example.Object;
 
 public class TrafficLight {
+
+    private String ID;
+    private String currentState;
+
+
+    public TrafficLight(String ID,String state){
+        this.ID = ID;
+        this.currentState = state;
+    }
+
+    public String getID(){
+        return  ID;
+    }
+
+    public String getCurrentState(){
+        return currentState;
+    }
+
+    public void setCurrentState(String newState){
+        this.currentState = newState;
+    }
 }
